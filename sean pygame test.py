afa2f4c0b223c000a7a3fab30b09b162920b2106
sf318 <sean.firth.sf@gmail.com>
2015-02-12 17:38:13 +0000

    Close button
    The exit button should now work

--- sean pygame test.py
+++ sean pygame test.py
@@ -3,3 +3,12 @@ pygame.init()
 size = (700, 700)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Creme Egg Project")
+
+done = False
+clock = pygame.time.Clock()
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
