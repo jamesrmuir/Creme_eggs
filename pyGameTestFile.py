471502711b0b257ed68a53ca78e8b450b72f5c18
JamesMuir <JRMuir09@gmail.com>
2015-02-12 22:12:15 +0000

    Finished pyGameTest
    pyGameTest V1 prints out user input.

--- pyGameTestFile.py
+++ pyGameTestFile.py
@@ -10,4 +10,30 @@ pygame.init()
 
 #Defines screen
 gameDisplay = pygame.display.set_mode((800,600))
-pygame.display.set_caption("A test game")
+pygame.display.set_caption("A creme eggs test game")
+
+#Set's clock rate
+clock = pygame.time.Clock()
+
+
+#Main game loop
+crashed = False
+
+while not crashed:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+            
+        print(event)
+
+    pygame.display.update()
+    clock.tick(60) #Sets FPS
+
+#Quits the game
+pygame.quit()
+quit()
+
+
+
+    
