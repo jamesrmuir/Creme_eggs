264ecfc2a0329986fa35b4407e08e36df1eb7630
JamesMuir <JRMuir09@gmail.com>
2015-02-12 21:36:06 +0000

    Started work on pyGame test file
    Added pyGameTestFile V1.0

---
+++ pyGameTestFile.py
@@ -0,0 +1,13 @@
+#pyGame test
+__author__ = "James Muir"
+__version__ = "1.0"
+
+#Imports modules
+import pygame
+
+#Initialises pygame
+pygame.init() 
+
+#Defines screen
+gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption("A test game")
