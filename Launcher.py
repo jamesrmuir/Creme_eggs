793f45390d3a3d3784deca5d61f8b78ce4354ce5
JamesMuir <JRMuir09@gmail.com>
2015-02-26 21:53:18 +0000

    Launcher
    Continued launcher

--- Launcher.py
+++ Launcher.py
@@ -5,16 +5,26 @@ from tkinter import *
 import commonVariables
 
 #Functions=====================================================================#
-def main():
-    #Sets TK window
-    mainWindow = Tk()
-
+def mainMenu():
     #Title
     title = Label(mainWindow, text="Main Menu")
     title.grid(row=0, column=0)
 
     #Changes the resolution
-    changeResolution = Button(mainWindow, text="Change Resolution")
+    changeResolution = Button(mainWindow, text="Change Resolution", command=resolutionMenu)
+    changeResolution.grid(row=1, column=0)
+    
+def resolutionMenu():
+    #Title
+    title = Label(mainWindow, text="Change resolution")
+    title.grid(row=0, column=0)
 
+    back = Button(mainWindow, text="Back", command=mainMenu)
+    back.grid(row=1, column=0)
+    
+    
 #Program=======================================================================#
-main()
+#Sets TK window
+mainWindow = Tk()
+
+mainMenu()
