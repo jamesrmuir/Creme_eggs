05c1c4b0b9a6ba841a1d5a8e8c6f85f7177fa9f8
sf318 <sean.firth.sf@gmail.com>
2015-03-03 07:23:39 +0000

    Profile Menu Concept
    Profile menu- asks the user to pick a username with an entry box and a
    confirm button.

---
+++ Profile Menu Concept.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+mainWindow = Tk()
+mainWindow.title("Pi Thon Profile Menu")
+mainWindow.minsize(width=400,height=400)
+mainWindow.config(bg = "light green")
+
+def userNamePopUp():
+    userName = ent1.get()
+    messagebox.showinfo(title="Username changed",message="Your new username is "+userName)
+
+
+
+lab1 = Label(mainWindow, text="Create a new username:", fg = "blue", bg = "light green", font=('times',30, 'bold'))
+lab1.pack()
+
+ent1 = Entry(mainWindow, fg = "green", bg = "light green", font=('times',20, 'bold'))
+ent1.pack()
+
+but1 = Button(mainWindow,text="Confirm", fg = "blue", bg = "light green", command=userNamePopUp, font=('times',15, 'bold'))
+but1.pack()
+
+mainloop()
